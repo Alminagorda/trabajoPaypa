811f25c7abea4a4238c867e50899960c82f641bd
Alminagorda <gala.alminagorda@gmail.com>
2024-04-10 23:09:20 -0500

    mi primer commit

---
+++ src/main/java/com/upc/demotf/DemoTfApplication.java
@@ -0,0 +1,13 @@
+package com.upc.demotf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/demotf/DemoTfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.demotf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
